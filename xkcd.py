cb9bf31c5fe2d148607f182a8d8b3507a52c65df
andrewmk <1872642+andrewmk@users.noreply.github.com>
2021-12-04 23:50:22 +0000

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -3,21 +3,26 @@ import PySimpleGUI as sg
 import tkinter
 from xkcdpass import xkcd_password as xp
 
-layout = [
-    [sg.Col(layout=[[]], s=(None, 80))],
-    [sg.Text('XKCD Password Generator', k='-TITLE-', p=0, s=30, font='Courier 20', justification='centre')],
-    [sg.Col(layout=[[]], s=(None, 80))],
-    [sg.Input(k='-1-', p=0, s=20, font='Courier 20')],
-    [sg.Col(layout=[[]], s=(None, 80))],
-    [sg.Input(k='-2-', p=0, s=20, font='Courier 20')],
-    [sg.Col(layout=[[]], s=(None, 80))],
-    [sg.Input(k='-3-', p=0, s=20, font='Courier 20')],
-    [sg.Col(layout=[[]], s=(None, 80))],
-    [sg.Input(k='-4-', p=0, s=20, font='Courier 20')],
-    [sg.Col(layout=[[]], s=(None, 100))],
-    [sg.Input(k='-out-', p=0, s=40, font='Courier 16', justification='centre')],
-    [sg.Col(layout=[[]], s=(None, 60))],
+def C(height):
+    return [sg.Col(layout=[[]], s=(None, height))]
 
+def W(k, fs=20, w=20, just='left'):
+    return [sg.Input(k=f'-{k}-', p=0, s=w, font=f'Courier {fs}', justification=just)],
+
+layout = [
+    C(80),
+    [sg.Text('XKCD Password Generator', k='-TITLE-', p=0, s=30, font='Courier 20', justification='centre')],
+    C(80),
+    W(1),
+    C(80),
+    W(2),
+    C(80),
+    W(3),
+    C(80),
+    W(4),
+    C(100),
+    W('out', fs=18, w=32, just='centre'),
+    C(60),
     [sg.Button('Generate', k='-GEN-', p=0, button_color='white on red')]
 ]
 
@@ -27,9 +32,8 @@ window = sg.Window('XKCD password generator', layout, element_justification='cen
 filename = tkinter.PhotoImage(file = "battery.png")
 background_label = tkinter.Label(window.TKroot, image=filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-# Cover everything with image
 background_label.lift()
-# Make UI visible again above image
+
 window['-TITLE-'].ParentRowFrame.lift()
 window['-1-'].ParentRowFrame.lift()
 window['-2-'].ParentRowFrame.lift()
@@ -38,12 +42,12 @@ window['-4-'].ParentRowFrame.lift()
 window['-out-'].ParentRowFrame.lift()
 window['-GEN-'].ParentRowFrame.lift()
 
-
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
     if event == '-GEN-':
+
         # create a wordlist from the default wordfile
         # use words between 5 and 8 letters long
         wordfile = xp.locate_wordfile()
